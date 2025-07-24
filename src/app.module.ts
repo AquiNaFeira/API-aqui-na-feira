e8366bfde6304b6d2aba8d2d62bb2862161138f3
Wyldson Marllon <161338581+WyldSLA@users.noreply.github.com>
2025-07-24 17:29:15 -0300

    create folders

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,19 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { AthModule } from './auth/auth.module';
+import { UserModule } from './user/user.module';
+import { ClientModule } from './client/client.module';
+import { FeiranteModule } from './feirante/feirante.module';
+import { BancoModule } from './banco/banco.module';
+import { EvaluationModule } from './evaluation/evaluation.module';
+import { FavoriteModule } from './favorite/favorite.module';
+import { NotificationModule } from './notification/notification.module';
+import { TagsModule } from './tags/tags.module';
+import { DatabaseModule } from './database/database.module';
 
 @Module({
-  imports: [],
+  imports: [AthModule, UserModule, ClientModule, FeiranteModule, BancoModule, EvaluationModule, FavoriteModule, NotificationModule, TagsModule, DatabaseModule],
   controllers: [AppController],
   providers: [AppService],
 })
